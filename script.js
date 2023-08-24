afbe37d98c55f5481fa14d88ec297641d3e05936
Ryan <98854650+ryalee@users.noreply.github.com>
2023-08-24 10:05:20 -0300

    update in css & add a javascript file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Passar comentario de usuario
+function nextItem() {
+  const item1 = document.querySelector('.fb1');
+  const item2 = document.querySelector('.fb2');
+
+  item1.classList.add('active');
+  item2.classList.add('active');
+}
+
+function prevItem() {
+  const item1 = document.querySelector('.fb1');
+  const item2 = document.querySelector('.fb2');
+
+  item1.classList.remove('active');
+  item2.classList.remove('active');
+}
